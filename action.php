a2abe4bef27f972216d475eba14a277dd5a7260a
natashashanae94 <natashashanae94@gmail.com>
2019-10-26 16:52:53 -0400

    My first commit in Github

---
+++ action.php
@@ -0,0 +1,50 @@
+
+<?php
+  require 'config.php';
+
+  if(isset($_POST['action'])) {
+    $sql="SELECT * FROM food WHERE meal!=''";
+
+    /*MEAL*/
+    if(isset($_POST['meal'])){
+      $meal = implode("','", $_POST['meal']);
+      $sql .= "AND meal IN ('".$meal."')";
+    }
+
+    /*DIET*/
+    if(isset($_POST['diet'])){
+      $diet = implode("','", $_POST['diet']);
+      $sql .= "AND diet IN ('".$diet."')";
+    }
+
+    $result = $conn->query($sql);
+    $output = '';
+
+    if($result->num_rows>0) {
+      while($row=$result->fetch_assoc()) {
+        $output .=
+        '<div class="col-md-4 mb-2">
+                <div class="card-deck">
+                  <div class="card shadow">
+                    <img src="'.$row['product_image'].'" class="card-img-top">
+                    <div class="card-img-overlay">
+                    </div>
+                    <div class="card-body" style="margin-top: 25px;">
+                      <h4 class="card-title">'.$row['product_name'].'</h4>
+                      <p>
+                        '.$row['meal'].'<br>
+                        $'.$row['price'].'<br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>';
+      }
+
+    } else {
+      $output = "<h3>No Products Found</h3>";
+    }
+    echo $output;
+  }
+
+?>
